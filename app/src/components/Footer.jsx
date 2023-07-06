3aadb9cb636ee489453e27d14547d9b6defb02f0
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-06 10:32:19 -0600

    with of the container changed

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div class="w-full flex flex-col text-center ">
+    <div class="w-full flex flex-col text-center p-5">
         Challenge by <a className="challengeby" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
         Coded by <a href="#" className="bodedby">Tomas Emanuel Ramirez Abarca</a>
     </div>
