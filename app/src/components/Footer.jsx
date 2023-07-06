9a0a6e14a61a83eb42886610564e89878bb44577
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-07-06 10:51:34 -0600

    Linkedin added in url frontend mentor

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <div class="w-full flex flex-col text-center p-5">
-        Challenge by <a className="challengeby" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
-        Coded by <a href="https://www.linkedin.com/in/emanuelramirezabarca/" target="blank" className="bodedby">Tomas Emanuel Ramirez Abarca</a>
+        Challenge by <a className="challengeby" href="https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV" rel="noreferrer" target="_blank">Frontend Mentor</a>
+        Coded by <a href="https://www.linkedin.com/in/emanuelramirezabarca/" target="blank" rel="noreferrer" className="bodedby">Tomas Emanuel Ramirez Abarca</a>
     </div>
   )
 }
